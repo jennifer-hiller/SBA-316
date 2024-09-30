494b69d13222e5327c15844ba64efd46e73cd22d
Jennifer Hiller <jennhi@gmail.com>
2024-09-30 10:40:43 -0700

    What I have so far

---
+++ script.js
@@ -0,0 +1,38 @@
+const ulElm = document.getElementById("list");
+function countLiElms() {
+  const liElms = ulElm.querySelectorAll("li");
+  let count = 0;
+  liElms.forEach((li) => {
+    count++;
+  }); // I know there was a better way around this but I wanted to get that loop in
+  document.getElementById("total").textContent = count;
+}
+document.addEventListener("DOMContentLoaded", countLiElms);
+document.getElementById("createNew").addEventListener("click", () => {
+  const li = document.createElement("li");
+  const count = ulElm.querySelectorAll("li").length;
+  li.textContent = `item${count + 1}`;
+  document.getElementById("list").appendChild(li);
+  countLiElms();
+});
+document.getElementById("list").addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("completed");
+  }
+});
+const form1Elm = document.getElementById("form1");
+form1Elm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const inputElm = document.getElementById("name");
+  const name = inputElm.value;
+  alert(`Hello, ${name}!`);
+});
+const radioElm = document.getElementById("modify");
+const checkBoxElm = document.getElementById("disable");
+checkBoxElm.addEventListener("change", () => {
+  radioElm.disabled = checkBoxElm.checked;
+});
+const screenSizeButton = document.querySelector(".screenSizeButton");
+screenSizeButton.addEventListener("click", () => {
+  alert(`The screen size is ${window.innerWidth}x${window.innerHeight}`);
+});
